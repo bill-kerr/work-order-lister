8709fd53cb79cabf815b3c8245572d5f908d4a9d
Bill Kerr <48515765+bk7987@users.noreply.github.com>
2020-06-26 11:02:19 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+### Work Order Lister
+The Work Order Lister program is intended to be run inside a directory whose subdirectories are named beginning with the "GR4_" prefix.
+The program will scan these folders for a .txt file and add both the name of the directory and the contents of the .txt file to a file named "work_orders.txt" in the parent directory.
+The "work_orders.txt" file will list all work orders and their descriptions for easy viewing.
---
+++ main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func main() {
+	workOrders := map[string]string{}
+
+	path, _ := filepath.Abs(filepath.Dir(os.Args[0]))
+	files, err := ioutil.ReadDir(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, f := range files {
+		if strings.HasPrefix(f.Name(), "GR4_") && f.IsDir() {
+			description := getDescription(f.Name())
+			workOrders[f.Name()] = description
+		}
+	}
+
+	writeToFile(workOrders)
+}
+
+func getDescription(directory string) string {
+	path, _ := filepath.Abs(filepath.Dir(os.Args[0]) + "\\" + directory + "\\")
+	files, err := ioutil.ReadDir(path)
+	log.Print(files)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, f := range files {
+		if strings.HasSuffix(f.Name(), ".txt") {
+			path, _ = filepath.Abs(filepath.Dir(os.Args[0]) + "\\" + directory + "\\" + f.Name())
+			return readTextFile(path)
+		}
+	}
+
+	return ""
+}
+
+func readTextFile(filePath string) string {
+	file, err := os.Open(filePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	return getFileContents(file)
+}
+
+func getFileContents(file *os.File) string {
+	filebytes, err := ioutil.ReadAll(file)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return parseBytes(filebytes)
+}
+
+func parseBytes(filebytes []byte) string {
+	description := string(filebytes)
+	description = strings.TrimSuffix(description, "\n")
+	description = strings.Replace(description, "\r\n", ", ", -1)
+	return description
+}
+
+func writeToFile(workOrders map[string]string) {
+	fileContents := ""
+
+	for key, elem := range workOrders {
+		fileContents += key + " - " + elem
+	}
+
+	err := ioutil.WriteFile("work_orders.txt", []byte(fileContents), 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
